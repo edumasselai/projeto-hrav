0633bc02e13a211ac34ba6fde8574e6f90630dc7
edumasselai <eduardo.masselai@gmail.com>
2024-10-20 21:08:57 -0300

    ok

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@ $perguntas = pg_fetch_all($result);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Avaliação de Serviços HRAV</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="public/css/estilo.css">
 </head>
 <body>
     <h1>Avaliação de Serviços</h1>
